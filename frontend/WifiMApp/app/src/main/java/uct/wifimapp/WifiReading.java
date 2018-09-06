0f35989648c9460ec143910530c39b58680d6d44
Tawanda <tawa.0.nda@gmail.com>
2018-09-06 17:41:12 +0200

    update models

--- frontend/WifiMApp/app/src/main/java/uct/wifimapp/WifiReading.java
+++ frontend/WifiMApp/app/src/main/java/uct/wifimapp/WifiReading.java
@@ -1,18 +1,90 @@
 package uct.wifimapp;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * A single wifi signal reading taken at a specific set of coordinates.
  */
 public class WifiReading {
 
+    @SerializedName("bssid")
+    private String bssid; //Unique mac address of wireless router
+
+    @SerializedName("signalStrength")
+    private int strength; //level of signal strength
+
+    @SerializedName("location")
+    private Location location; //location of the router
+
+    @SerializedName("timestamp")
+    private long timestamp;
+
     private double latitude;
     private double longitude;
-    private int strength;
 
-    public WifiReading( double _latitude, double _longitude, int _strength){
-        latitude = _latitude;
-        longitude = _longitude;
-        strength = _strength;
+    public WifiReading() { }
+
+    public WifiReading(double _latitude, double _longitude, int _strength){
+        this.latitude = _latitude;
+        this.longitude = _longitude;
+        this.strength = _strength;
+    }
+
+    public WifiReading(String bssid, double _latitude, double _longitude, int _strength, long timestamp){
+        this.bssid = bssid;
+        this.latitude = _latitude;
+        this.longitude = _longitude;
+        this.strength = _strength;
+        this.location = new Location(latitude,longitude);
+        this.timestamp = timestamp;
+    }
+
+    public String getBssid() {
+        return bssid;
+    }
+
+    public void setBssid(String bssid) {
+        this.bssid = bssid;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
     }
 
     public double latitude(){
@@ -27,8 +99,16 @@ public class WifiReading {
         return strength;
     }
 
-    public String toString(){
-        return latitude + " " + longitude + " " + strength;
+    @Override
+    public String toString() {
+        return "WifiReading{" +
+                "bssid='" + bssid + '\'' +
+                ", strength=" + strength +
+                ", location=" + location +
+                ", timestamp=" + timestamp +
+                ", latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
     }
 
 }
